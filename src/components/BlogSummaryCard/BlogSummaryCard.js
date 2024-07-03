87abdeae9eb73c375e271c66722bb053a326ddec
ssb <sb1664@gmail.com>
2024-07-03 17:33:02 +0100

    Ex. 2: ✅ - load blog post and display MDX

--- src/components/BlogSummaryCard/BlogSummaryCard.js
+++ src/components/BlogSummaryCard/BlogSummaryCard.js
@@ -6,12 +6,7 @@ import Card from '@/components/Card';
 
 import styles from './BlogSummaryCard.module.css';
 
-function BlogSummaryCard({
-  slug,
-  title,
-  publishedOn,
-  abstract,
-}) {
+function BlogSummaryCard({ slug, title, publishedOn, abstract }) {
   const href = `/${slug}`;
   const humanizedDate = format(
     new Date(publishedOn),
@@ -20,18 +15,14 @@ function BlogSummaryCard({
 
   return (
     <Card className={styles.wrapper}>
-      <Link href={href} className={styles.title}>
+      <Link href={href} className={styles.title} prefetch={false}>
         {title}
       </Link>
       <time dateTime={publishedOn}>{humanizedDate}</time>
       <p>
         {abstract}{' '}
-        <Link
-          href={href}
-          className={styles.continueReadingLink}
-        >
-          Continue reading{' '}
-          <span className={styles.arrow}>→</span>
+        <Link href={href} className={styles.continueReadingLink}>
+          Continue reading <span className={styles.arrow}>→</span>
         </Link>
       </p>
     </Card>
