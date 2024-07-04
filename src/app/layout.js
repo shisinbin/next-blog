f04dc213d2ddf15f3f78fc8ca47986f1aa418781
ssb <sb1664@gmail.com>
2024-07-04 18:43:19 +0100

    Add thin motion preferences wrapper

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,8 +4,10 @@ import clsx from 'clsx';
 
 import { LIGHT_TOKENS, DARK_TOKENS, BLOG_TITLE } from '@/constants';
 
+import RespectMotionPreferences from '@/components/RespectMotionPreferences';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+
 import './styles.css';
 
 const mainFont = Work_Sans({
@@ -31,19 +33,21 @@ function RootLayout({ children }) {
   const theme = 'light';
 
   return (
-    <html
-      lang='en'
-      className={clsx(mainFont.variable, monoFont.variable)}
-      data-color-theme={theme}
-      style={theme === 'light' ? LIGHT_TOKENS : DARK_TOKENS}
-      suppressHydrationWarning // to circumvent console warning for data-google-analytics-opt-out
-    >
-      <body>
-        <Header theme={theme} />
-        <main>{children}</main>
-        <Footer />
-      </body>
-    </html>
+    <RespectMotionPreferences>
+      <html
+        lang='en'
+        className={clsx(mainFont.variable, monoFont.variable)}
+        data-color-theme={theme}
+        style={theme === 'light' ? LIGHT_TOKENS : DARK_TOKENS}
+        suppressHydrationWarning // to circumvent console warning for data-google-analytics-opt-out
+      >
+        <body>
+          <Header theme={theme} />
+          <main>{children}</main>
+          <Footer />
+        </body>
+      </html>
+    </RespectMotionPreferences>
   );
 }
 
